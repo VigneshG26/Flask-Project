9501964784565ab5984a7625d9499532a6e2ee2a
VigneshG26 <vigneshganesan2k1@gmail.com>
2024-07-16 01:32:23 +0530

    dynamic product lis page achived

--- app.py
+++ app.py
@@ -4,6 +4,222 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Toys = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/kids1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/kids1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Laptop = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/laptop1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/laptop1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Shoes = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/shoes1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/shoes1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Mobile = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/mobile1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/mobile1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Headphones = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/headphones1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/headphones1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Women = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/women1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/women1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Speaker = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/speaker1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/speaker1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+CollegeBag = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/cbag1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/cbag1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Handbags = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/hbags1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/hbags1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Men = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/men1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/men1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+TShirt = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/tshirt1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/tshirt1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
+Watch = [
+    {
+        'id':1,
+        'thumbnail': '../static/images/watch1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    },
+    {
+        'id':2,
+        'thumbnail': '../static/images/watch1.png',
+        'brand':'funskool',
+        'offer_pice':'₹500',
+        'actula_price':'₹1,000',
+        'offer_percentage': '50% off'
+    }
+]
 
 
 @app.route('/')
@@ -12,7 +228,31 @@ def index():
 
 @app.route('/products/<name>')
 def products(name):
-    return render_template('specific_products.html',cat = name)
+    if name == 'Toys':
+        categories = Toys
+    elif name =='Laptop':
+        categories = Laptop
+    elif name =='Shoes':
+        categories = Shoes
+    elif name =='Mobile':
+        categories = Mobile
+    elif name =='Headphones':
+        categories = Headphones
+    elif name =='Women':
+        categories = Women
+    elif name =='Speaker':
+        categories = Speaker
+    elif name =='CollegeBag':
+        categories = CollegeBag
+    elif name =='Handbags':
+        categories = Handbags
+    elif name =='Men':
+        categories = Men
+    elif name =='T-shirts':
+        categories = TShirt
+    elif name =='Watch':
+        categories = Watch
+    return render_template('specific_products.html',categories = categories, title = name)
 
 
 
