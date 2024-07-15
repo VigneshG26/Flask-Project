8dea0408bd5481db29696fc77e35f1f37ca5dbdd
VigneshG26 <vigneshganesan2k1@gmail.com>
2024-07-15 22:51:25 +0530

    specific product page added

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+@app.route('/products/<name>')
+def products(name):
+    return render_template('specific_products.html',cat = name)
 
 
 
